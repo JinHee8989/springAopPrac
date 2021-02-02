fdb69ed5d1d2c6f53accae8f97a2889b5eac4160
jinheebyeon <wlsgml8989@gmail.com>
2021-02-02 22:33:57 +0900

    # 테스트코드 오류 수정중

--- src/test/java/com/springaop/test/controller/BoardControllerTest.java
+++ src/test/java/com/springaop/test/controller/BoardControllerTest.java
@@ -1,27 +1,45 @@
 package com.springaop.test.controller;
 
+import com.springaop.test.service.BoardService;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = BoardController.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class BoardControllerTest {
 
-    @Autowired  //스프링이 관리하는 Bean을 주입받음
-    private MockMvc mvc;    // 웹API를 테스트할 때 사용, 스프링MVC테스트의 시작점임
+    private MockMvc mvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Before     //매번 테스트가 시작되기 전에 MockMvc인스턴스를 생성
+    public void setup(){
+        mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
+    }
+
+    @LocalServerPort
+    private int port;
 
     @Test
     void 노크테스트() throws Exception {
-
-        mvc.perform(get("/knock"))
+        String url = "http://localhost:"+port+"/api/knock";
+        mvc.perform(get(url))
                 .andExpect(status().isOk())
                 .andExpect(content().string("똑똑"));
     }
